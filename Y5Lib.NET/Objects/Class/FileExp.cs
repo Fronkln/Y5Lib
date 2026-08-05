cbaa4a23dac18a30153a6d8b78095faae25196cc
Jhrino <orhankaymak@windowslive.com>
2026-08-05 19:45:04 +0300

    Check skills and scenario state

---
+++ Y5Lib.NET/Objects/Class/FileExp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Y5Lib.Unsafe;
+
+namespace Y5Lib
+{
+    public unsafe static class FileExp
+    {
+        private static IntPtr* m_fileExp;
+
+        private delegate void FileExpGetScenarioStateForSkill(IntPtr a1, PlayerID playerID, int skillID, ref short in_state_group, ref short in_state);
+
+        static FileExp()
+        {
+            m_fileExp = (IntPtr*)CPP.ResolveRelativeAddress(CPP.PatternSearch("48 8B 0D ? ? ? ? E8 ? ? ? ? 89 03 0F B7 45"), 7);
+            _FGetScenarioStateForSkill = Marshal.GetDelegateForFunctionPointer<FileExpGetScenarioStateForSkill>(CPP.PatternSearch("48 8B 81 ? ? ? ? 33 C9 48 83 C0 ? 44 8B D1"));
+        }
+
+        private static FileExpGetScenarioStateForSkill _FGetScenarioStateForSkill;
+        public static (short, short) GetScenarioStateForSkill(int skillID, PlayerID playerID)
+        {
+            IntPtr fileExp = *m_fileExp;
+
+            if (fileExp == IntPtr.Zero)
+                return (-1, -1);
+
+            short stateGroup = -1;
+            short state = -1;
+
+            _FGetScenarioStateForSkill(fileExp, playerID + 1, skillID, ref stateGroup, ref state);
+
+            return (stateGroup, state);
+        }
+
+        public static bool CheckSkill(int skillID, PlayerID playerID)
+        {
+            var state = GetScenarioStateForSkill(skillID, playerID);
+
+            if (state.Item1 == -1 || state.Item2 == -1)
+                return false;
+
+            return ScenarioStateManager.CheckState(state.Item1, state.Item2, true);
+        }
+    }
+
+}
--- Y5Lib.NET/Objects/Class/ScenarioStateManager.cs
+++ Y5Lib.NET/Objects/Class/ScenarioStateManager.cs
@@ -8,6 +8,15 @@ namespace Y5Lib
     {
         public static IntPtr ActiveStateBuffer = CPP.ResolveRelativeAddress(CPP.PatternSearch("48 8D 0D ? ? ? ? 42 8B 14 91"), 7);
 
+        private static IntPtr m_scenarioStateManager;
+        private delegate bool ScenarioStateManagerCheckState(IntPtr a1, int stateGroup, int state, bool unknown = true);
+
+        static ScenarioStateManager()
+        {
+            _FCheckState = Marshal.GetDelegateForFunctionPointer<ScenarioStateManagerCheckState>(CPP.PatternSearch("C1 E2 ? 41 03 D0"));
+            m_scenarioStateManager = CPP.ResolveRelativeAddress(CPP.PatternSearch("48 8B 0D ? ? ? ? 44 0F B7 C0 E8 ? ? ? ? 48 8B 5C 24 ? 48 83 C4 ? 5F"), 7);
+        }
+
         public static byte[] GetActiveStateBuffer()
         {
             //We believe the full buffer for Yakuza 5 scenario state is 0x6400 bytes.
@@ -23,9 +32,15 @@ namespace Y5Lib
             Marshal.Copy(buffer, 0, ActiveStateBuffer, buffer.Length);
         }
 
+        private static ScenarioStateManagerCheckState _FCheckState;
         public static bool CheckState(int stateGroup, int state, bool unknown = true)
         {
-            return false;
+            IntPtr stateManager = Marshal.ReadIntPtr(m_scenarioStateManager);
+
+            if (stateManager == IntPtr.Zero)
+                return false;
+
+            return _FCheckState(stateManager, stateGroup, state, unknown);
         }
     }
 }
